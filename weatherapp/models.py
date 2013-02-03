41e5ea85e61479045c73b47e09410b4b2f85e316
ryan <ryan@tacticalcodemonkeys.com>
2013-02-02 19:04:10 -0500

    database fixes

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -2,12 +2,13 @@ from weatherapp import db
 from datetime import datetime
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	facebook_id = db.Column(db.String(120), primary_key=True)
 	name = db.Column(db.String(80))
 	email = db.Column(db.String(120), unique=True)
 	zipcode = db.Column(db.String(5))
 
-	def __init__(self, name, email, zipcode):
+	def __init__(self, facebook_id, name, email, zipcode):
+		self.facebook_id = facebook_id
 		self.name = name
 		self.email = email
 		self.zipcode = zipcode
@@ -25,7 +26,7 @@ class Location(db.Model):
 	def __init__(self, zipcode, last_updated):
 		self.zipcode = zipcode
 		self.last_updated = last_updated
-		#self.notification_sent = datetime.now()
+		self.notification_sent = datetime.now()
 
 	def __repr__(self):
 		return '<zipcode %r>' % self.zipcode
