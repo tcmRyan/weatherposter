f9bba907b5b6500fcc718bcefae8436207529350
ryan <ryan@tacticalcodemonkeys.com>
2013-02-16 14:58:37 -0500

    fixed url

--- weatherapp/run_weather.py
+++ weatherapp/run_weather.py
@@ -3,7 +3,7 @@
 import requests
 
 def update_db():
-	requests.post('http://weatherposter.herokuapp.com')
+	requests.post('http://weatherposter.herokuapp.com/weathertest/')
 
 if __name__ == '__main__':
 	update_db()
